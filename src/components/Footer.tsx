e5c4141777a7c6fc522dc8ad594898217ec11138
AndreLMiller <79182039+AndreLMiller@users.noreply.github.com>
2024-11-21 13:49:48 -1000

    landingPage

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,15 +3,23 @@ import { Col, Container } from 'react-bootstrap';
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 const Footer = () => (
   <footer className="mt-auto py-3 bg-light">
+    <div className="text-center">
+      <p>
+        The Club Pub is a platform designed to help you find the right club for you. You can browse
+        through the list of clubs and find one that interests you. You can also add a club to the
+        list if you are a club owner.
+      </p>
+      <hr />
+    </div>
     <Container>
       <Col className="text-center">
-        Department of Information and Computer Sciences
+        Site Created and Managed By: 404JobsNotFound
         <br />
         University of Hawaii
         <br />
         Honolulu, HI 96822
         <br />
-        <a href="http://ics-software-engineering.github.io/nextjs-application-template">Template Home Page</a>
+        <a href="https://404jobsnotfound.github.io/">Project Home Page</a>
       </Col>
     </Container>
   </footer>
