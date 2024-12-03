c5ad399aff0c8901e84acf0126a578080782889d
Patrick Ariola <patrickariola172@gmail.com>
2024-12-03 12:26:51 -1000

    Edited prisma file and db seeding (#15)
    * Edited prisma file and db seeding
    
    Made the necessary parts for the database table
    
    * Fixed firstName null
    
    in schema.prisma

---
+++ prisma/migrations/20241203113352_club/migration.sql
@@ -0,0 +1,34 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Stuff` table. If the table is not empty, all the data it contains will be lost.
+  - Added the required column `firstName` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `lastName` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterEnum
+ALTER TYPE "Role" ADD VALUE 'CLUB_OWNER';
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "firstName" TEXT NOT NULL,
+ADD COLUMN     "lastName" TEXT NOT NULL;
+
+-- DropTable
+DROP TABLE "Stuff";
+
+-- DropEnum
+DROP TYPE "Condition";
+
+-- CreateTable
+CREATE TABLE "Club" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "meetingTime" TEXT NOT NULL,
+    "meetingLocation" TEXT NOT NULL,
+    "interestAreas" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "admins" TEXT NOT NULL,
+
+    CONSTRAINT "Club_pkey" PRIMARY KEY ("id")
+);
